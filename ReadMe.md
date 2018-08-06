743710edc297392ae05f806a526f91b0eb03bb0f
Ellen <ellen.bacerra@gmail.com>
2018-08-05 19:46:39 -0500

    update

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Psychic Game
+
+The object of the game is to guess any letters from A-Z against the computer. You can score by guessing the matching letters within the 10 tries. Once a letter is guessed, the game should automatically reset. Good luck!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,9 @@
 
 // 
 //need variables to declare
-
 var compCounter = 0;
 var userCounter = 0;
-var guessLeft = 0;
-
+var guessLeft = 9;
 
 
 function guess() {
@@ -14,31 +12,104 @@ function guess() {
 
     // computer chooses the random letters 1-26
     computerChoice = compLetters[Math.floor(Math.random() * 26)];
-    // var compNum = computerChoice;
-    // onkeyup function for the user to click
 
+
+
+    // onkeyup function for the user to click
     document.onkeyup = function (event) {
         var userGuess = event.key;
 
-        // condition for the game
+
+
+        // condition
         if (computerChoice === userGuess || userGuess === computerChoice) {
-            document.getElementById('win').innerHTML = userCounter++;
+            document.getElementById('result').innerHTML = userCounter++;
+            guess();
+            // I tried to apply the callback to auto restart the game and it's not working.
 
         } else if (computerChoice > userGuess || userGuess < computerChoice) {
-            document.getElementById('lose').innerHTML = compCounter++ + guessLeft--;
+            document.getElementById('lose').innerHTML = compCounter++
+            guessLeft--;
 
         }
     }
-
+    // ---- see example from printed on HTML page on "Guesses so far" section: k, d, m, g, j, c, n, v, p, e,
+    // I got this to list all of the pressed keys but when I had added multiple events to do this, it slowed down the results. There's gotta be a better way to do this.. not sure if it's my internet connections but it's probably my coding :(
     document.onkeypress = function (event) {
-        guessedLetters = event.key
+        guessedLetter1 = event.key;
+        document.onkeypress = function (event) {
+            guessedLetter2 = event.key;
+            document.onkeypress = function (event) {
+                guessedLetter3 = event.key;
+                document.onkeypress = function (event) {
+                    guessedLetter4 = event.key;
+                    document.onkeypress = function (event) {
+                        guessedLetter5 = event.key;
+                        document.onkeypress = function (event) {
+                            guessedLetter6 = event.key;
+                            document.onkeypress = function (event) {
+                                guessedLetter7 = event.key;
+                                document.onkeypress = function (event) {
+                                    guessedLetter8 = event.key;
+                                    document.onkeypress = function (event) {
+                                        guessedLetter9 = event.key;
+                                        document.onkeypress = function (event) {
+                                            guessedLetter10 = event.key;
 
-        // print on HTML
-        document.getElementById('win').innerHTML = userCounter;
-        document.getElementById('lose').innerHTML = compCounter;
-        document.getElementById('Guessed').innerHTML = guessLeft;
-        document.getElementById('pressedkey').innerHTML = guessedLetters + ",";
+                                            // print on HTML
+                                            document.getElementById('result').innerHTML = userCounter;
+                                            document.getElementById('lose').innerHTML = compCounter;
+                                            document.getElementById('Guessed').innerHTML = guessLeft;
+                                            document.getElementById('pressedkey').innerHTML = guessedLetter1 + ", " + guessedLetter2 + ", " + guessedLetter3 + ", " + guessedLetter4 + ", " + guessedLetter5 + ", " + guessedLetter6 + ", " + guessedLetter7 + ", " + guessedLetter8 + ", "
+                                                + guessedLetter9 + ", " + guessedLetter10;
+
+
+                                        };
+
+                                    };
+
+                                };
+
+                            };
+
+                        };
+                    };
+                };
+
+            };
+
+        };
+
     };
 
 }
 guess();
+
+// The game is working fine except it's not auto restarting without refreshing the page. I couldn't figure out how to allign the results to my category list.. 
+/*
+****my code***
+Wins:
+1
+
+Loses:
+2
+
+Guesses Left:
+-2
+
+Your Guesses so far:
+k, d, m, g, j, c, n, v, p, e 
+
+---------------------------
+
+How it should be:
+Wins: 1
+
+Loses: 2
+
+Guesses Left: -2
+
+Your Guesses so far:
+k, d, m, g, j, c, n, v, p, e /*
+
+I also couldn't make it to stop at 10, it keeps going */
